222dfb6d54edfc82cb0e8ef0a6cf2d159962fbde
toon2301 <tonochudjak@gmail.com>
2025-10-02 19:27:49 +0200

    Aktualizácia backendu a frontend build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     optimizePackageImports: ['lucide-react'],
+  },
+  
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
   
   images: {
