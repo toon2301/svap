32d2bc2d33c45d658270c62af388ab9a80526127
toon2301 <tonochudjak@gmail.com>
2026-02-23 19:55:56 +0100

    3.2.2026

--- backend/accounts/views/google_oauth_simple.py
+++ backend/accounts/views/google_oauth_simple.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.conf import settings
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -24,6 +24,7 @@ logger = logging.getLogger(__name__)
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
+@authentication_classes([])
 def google_login_view(request):
     """
     Jednoduchá Google OAuth login view
@@ -117,6 +118,7 @@ def google_login_view(request):
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
+@authentication_classes([])
 def google_callback_view(request):
     """
     Google OAuth callback view
